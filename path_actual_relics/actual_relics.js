0b465101518fc40f77e6971a893b2765ed210b16
Kulikov <saha.ale00@mail.ru>
2025-08-04 11:25:05 +0500

    Update

--- path_actual_relics/actual_relics.js
+++ path_actual_relics/actual_relics.js
@@ -5,7 +5,11 @@ document.addEventListener('DOMContentLoaded', async () => {
  
   
 
+<<<<<<< HEAD
   const res = await fetch('relics.json');
+=======
+  const res = await fetch('../public/relics.json');
+>>>>>>> 4703ab5 (Update)
   const relics = await res.json();
 
   render(relics);
@@ -41,7 +45,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   const dateElem = document.getElementById('date');
 
   try {
-    const res = await fetch('/last_update.json');
+    const res = await fetch('../public/last_update.json');
     if (!res.ok) throw new Error('Не удалось загрузить last_update.json');
 
     const data = await res.json();
